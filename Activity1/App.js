863fd6ee6183ba0d140fbe3e62639d6d77aa9256
ohmyjeian <150905645+ohmyjeian@users.noreply.github.com>
2025-02-06 16:46:29 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+const App = () => (
+  <ScrollView>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+<Text >Scroll me plz</Text>
+       
+  </ScrollView>
+);
+
+export default App;
